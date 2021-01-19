89343dec2f4f5469f3809139fda5d7687c898164
Charles Buxton <cbuxton@Promontory.PromontoryClub.com>
2021-01-19 16:35:20 -0700

    about section progress

--- app/public/wp-content/themes/nhmc_theme/index.php
+++ app/public/wp-content/themes/nhmc_theme/index.php
@@ -2,8 +2,32 @@
 
 <div id="main">
   <div id="slider">
-    <?php echo do_shortcode('[smartslider3 slider="2"]');?>
-    <h1>Main Content Area</h1>
+    <?php echo do_shortcode('[smartslider3 slider="2"]'); ?>
+  </div>
+  <div>
+    <div id="divider"></div>
+    <div id="about-img"></div>
+    <div id="about-text">
+      <div class="scroll-divider"></div>
+      <p>New Horizons Maritime Center provides a dynamic hands-on, apprentice-based
+        learning environment, designed specifically for youth development and community
+        betterment. These programs have been developed from a concern for at-risk and
+        underserved youth who struggle to succeed in the traditional educational system.
+        Our goal is to provide life changing programs to those who would never have the
+        chance to experience something like this without us.
+      </p>
+      <p>
+        NHMC offers in-school boat building classroom instruction, after-school programs, educational
+        workshops, apprentice possibilities, boat building classes, sailing instruction and
+        a safe place to pursue productive and useful life changing educational activities.
+        Through boat building and the restoration of donated boats needing repair, and under the
+        careful supervision of NHMC staff and volunteers, youth groups and other interested people
+        in the community will learn a wide variety of trade and social skills necessary for success
+        in today’s competitive world. Participants will gain a greater sense of tradition, respect,
+        and societal values as they tap into the rich maritime traditions our country has to offer.
+        New interests will be developed and innate potential realized.
+      </p>
+    </div>
   </div>
 </div>
 
