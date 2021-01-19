af9fea77c1f6c4abd2068ffc5ae3744911c5c502
Charles Buxton <cbuxton@Promontory.PromontoryClub.com>
2021-01-19 12:44:35 -0700

    styling progress header

--- app/public/wp-content/themes/nhmc_theme/header.php
+++ app/public/wp-content/themes/nhmc_theme/header.php
@@ -4,24 +4,20 @@
   <title>NHMC</title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 
-<header>
+
   <div class="head-logo">
-    LOGO
   </div>
-
-      
-        <div class="nav-container"> 
-          <a title="HOME" href="/">HOME</a>
-          <a title="NHMC BOD" href="nhmc-bod/">NHMC BOD</a>
-          <a title="NHMC PROGRAMS" href="/">NHMC PROGRAMS</a>
-          <a title="SUPPORT" href="/">SUPPORT</a>
-          <a title="BLOG" href="blog/">BLOG</a>
-          <a title="CONTACT NHMC" href="contact-nhmc/">CONTACT NHMC</a>
-        </div>
-
+  <div class="nav-container">
+    <a title="HOME" href="/">HOME</a>
+    <a title="NHMC BOD" href="nhmc-bod/">NHMC BOD</a>
+    <a title="NHMC PROGRAMS" href="/">NHMC PROGRAMS</a>
+    <a title="SUPPORT" href="/">SUPPORT</a>
+    <a title="BLOG" href="blog/">BLOG</a>
+    <a title="CONTACT NHMC" href="contact-nhmc/">CONTACT NHMC</a>
+  </div>
 
   <body>
