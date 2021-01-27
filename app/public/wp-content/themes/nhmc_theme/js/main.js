4cf7bf03619af015b40848798a9b3a9538c174ff
Charles Buxton <chazbux@gmail.com>
2021-01-27 15:00:19 -0700

    form complete

--- app/public/wp-content/themes/nhmc_theme/js/main.js
+++ app/public/wp-content/themes/nhmc_theme/js/main.js
@@ -4,15 +4,13 @@ var successMsg = document.querySelector('#success-message')
 wpcf7Elm.addEventListener( 'wpcf7mailsent', function( event ) {
   wpcf7Elm.style.display = "none";
   successMsg.style.display = "block";
-
 }, false );
 
 wpcf7Elm.addEventListener( 'wpcf7invalid', function( event ) {
-  alert( "Invalid" );
+  alert( "Invalid Input" );
 }, false );
 
+wpcf7Elm.addEventListener( 'wpcf7mailfailed ', function( event ) {
+  alert( "Error: Message Not Sent" );
+}, false );
 
-// wpcf7invalid — Fires when an Ajax form submission has completed successfully, but mail hasn’t been sent because there are fields with invalid input.
-// wpcf7spam — Fires when an Ajax form submission has completed successfully, but mail hasn’t been sent because a possible spam activity has been detected.
-// wpcf7mailsent — Fires when an Ajax form submission has completed successfully, and mail has been sent.
-// wpcf7mailfailed — Fires when an Ajax form submission has completed successfully, but it has failed in sending mail.
