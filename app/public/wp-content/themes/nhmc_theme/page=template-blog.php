a632216157a222396fac79d986bd7f0cb5b081c5
Charles Buxton <chazbux@gmail.com>
2021-02-03 16:49:50 -0700

    blog loop function ONLINE

--- app/public/wp-content/themes/nhmc_theme/page=template-blog.php
+++ app/public/wp-content/themes/nhmc_theme/page=template-blog.php
@@ -3,6 +3,29 @@
 get_header();
 ?>
 
-<h1>Blog page - coming soon!</h1>
+<?php
+    $args = array(
+        'post_type' => 'post'
+    );
+
+    $post_query = new WP_Query($args);
+
+    if($post_query->have_posts() ) {
+        while($post_query->have_posts() ) {
+            $post_query->the_post();
+            ?>
+            <!-- DISPLAY INFO -->
+
+            
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+
+
+            <!-- ____________  -->
+            <?php
+            wp_reset_postdata();
+            }
+        }
+?>
 
 <?php get_footer(); ?>
